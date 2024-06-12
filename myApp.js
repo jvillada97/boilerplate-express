2a891d1817cb33de987566d8fbe4be0d7cd03be3
Juan Camilo Villada <j.villadap@uniandes.edu.co>
2024-06-12 05:41:38 +0000

    3 exercise finished

--- myApp.js
+++ myApp.js
@@ -2,6 +2,8 @@ let express = require('express');
 let app = express();
 
 //console.log("Hello World")
+const publicMiddleWare = __dirname + '/public'
+app.use('/public',express.static(publicMiddleWare))
 
 app.get("/", function(req, res){
     const absolutePath = __dirname + '/views/index.html'
