a3aa64dedb6882ccf1b61a0be2e1f3391646fbcf
Juan Camilo Villada <j.villadap@uniandes.edu.co>
2024-06-13 02:45:00 +0000

    exercise #7

--- myApp.js
+++ myApp.js
@@ -7,6 +7,7 @@ const publicMiddleWare = __dirname + '/public'
 app.use('/public',express.static(publicMiddleWare))
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.path} - ${req.ip}`)
+    next()
 })
 
 app.get("/", function(req, res){
